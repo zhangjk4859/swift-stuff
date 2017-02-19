be794f8e459b8701dc88e12560b1a3cac7aa9713
zhangjk4859 <573581325@qq.com>
2017-02-19 20:20:31 +0800

    no message

---
+++ code/mouseAndCat/mouseAndCat/Mouse.swift
@@ -0,0 +1,23 @@
+//
+//  Mouse.swift
+//  mouseAndCat
+//
+//  Created by 张俊凯 on 2017/2/18.
+//  Copyright © 2017年 张俊凯. All rights reserved.
+//
+
+import UIKit
+
+class Mouse: NSObject {
+    
+    var age:Int?
+    
+    func eat(){
+        print("老鼠在吃东西")
+    }
+    
+    func walk(){
+        print("老鼠在走路")
+    }
+
+}
---
+++ code/mouseAndCat/mouseAndCat/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  mouseAndCat
+//
+//  Created by 张俊凯 on 2017/2/18.
+//  Copyright © 2017年 张俊凯. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let mouse = Mouse()
+        mouse.walk()
+        mouse.eat()
+        
+        
+    }
+
+    
+
+
+}
+
